7e0b0d7a838e2c48aadee1876f42b2bb6dc60209
GumruAliyeva <13654@holbertonstudents.com>
2026-03-31 16:50:30 +0000

    hfndjkm

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10000, 10000)
-
+number = random.randint(0, 99)  # 0 ilə 99 arası
 if number > 0:
     print("{} is positive".format(number))
 elif number == 0:
