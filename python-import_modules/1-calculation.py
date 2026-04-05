94e029454624a00f04f4a78955f3da7407011709
GumruAliyeva <13654@holbertonstudents.com>
2026-04-05 13:48:01 +0000

    Task done

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import  calculator_1 from add, sub, nul, div
+if __name__ == "__main__":
+# bütün kod buraya
+add(a, b)
+sub(a, b)
+mul(a, b)
+div(a, b)
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
